95d32057172fae190b18dc2acdacf90047f70226
Bill Wang <bill.wang>
2020-06-14 00:27:12 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Usage
+
+This repo is used to create rancher and add a kubernetes cluster with kind (kubernetes in docker) in Rancher.
+
+# Create the stack
+
+```
+$ ./rkind.sh --create
+INFO: Launching Rancher container
+cec8ff9b437bfe9234c56001a21fe5d9ba8423e182d25ab6dbe0f2b06f92308b
+INFO: Rancher UI will be available at https://192.168.1.100:33486
+INFO: It might take few up to 60 seconds for Rancher UI to become available..
+INFO: While it's coming up, going to start KIND cluster
+No kind clusters found.
+INFO: Creating Kind cluster..
+Creating cluster "kind-for-rancher" ...
+ ✓ Ensuring node image (kindest/node:v1.17.0) 🖼
+ ✓ Preparing nodes 📦 📦 📦 📦
+ ✓ Writing configuration 📜
+ ✓ Starting control-plane 🕹️
+ ✓ Installing CNI 🔌
+ ✓ Installing StorageClass 💾
+ ✓ Joining worker nodes 🚜
+Set kubectl context to "kind-kind-for-rancher"
+You can now use your cluster with:
+
+kubectl cluster-info --context kind-kind-for-rancher
+
+Have a nice day! 👋
+### Next steps ###
+- Setup admin credentials in Rancher UI
+- Set "Rancher Server URL" to "https://192.168.1.100:33486" (should already be selected)
+  you may change it at any time in "Settings"
+- Import KIND cluster to Rancher (via https://192.168.1.100:33486/g/clusters/add?provider=import)
+  (select "Import Existing cluster" when adding a cluster)
+> To work around "Unable to connect to the server: x509: certificate signed by unknown authority"
+> use "curl --insecure" to get the manigest, piping it's output to
+> KUBECONFIG="$(kind get kubeconfig-path --name=kind-for-rancher)" kubectl apply -f -
+To shut everything down, use "./rkind.sh cleanup", or manually with
+docker rm -f rancher-for-kind; kind delete cluster kind-for-rancher
+```
+### destroy the stack
+
+```
+$ ./rkind.sh --destroy
+INFO: Destroying Rancher container..
+rancher-for-kind
+INFO: Destroying Kind cluster..
+Deleting cluster "kind-for-rancher" ...
+```
+
+### custom kind configuration
+
+If you'd like to change the kind configuration, please update its file [kind.yaml](kind.yaml). For details, go through https://kind.sigs.k8s.io/
---
+++ rkind.sh
@@ -0,0 +1,214 @@
+#!/usr/bin/env bash
+#
+# RKIND is a naive helper script to start KIND and Rancher Management Server
+# Forked with updates from https://gist.github.com/anapsix/25a5a66696f14806a4686ec1c707d2d2
+
+set -u
+set -o pipefail
+
+RANCHER_CONTAINER_NAME="rancher-for-kind"
+RANCHER_HTTP_HOST_PORT=$[$[RANDOM%9000]+30000]
+RANCHER_HTTPS_HOST_PORT=$[$[RANDOM%9000]+30000]
+: ${KIND_CLUSTER_NAME:="kind-for-rancher"}
+
+info() {
+  if [[ ${QUIET:-0} -eq 0 ]] || [[ ${DEBUG:-0} -eq 1 ]]; then
+    echo >&2 -e "\e[92mINFO:\e[0m $@"
+  fi
+}
+
+warn() {
+  if [[ ${QUIET:-0} -eq 0 ]] || [[ ${DEBUG:-0} -eq 1 ]]; then
+    echo >&2 -e "\e[33mWARNING:\e[0m $@"
+  fi
+}
+
+debug(){
+  if [[ ${DEBUG:-0} -eq 1 ]]; then
+    echo >&2 -e "\e[95mDEBUG:\e[0m $@"
+  fi
+}
+
+error(){
+  local msg="$1"
+  local exit_code="${2:-1}"
+  echo >&2 -e "\e[91mERROR:\e[0m $1"
+  if [[ "${exit_code}" != "-" ]]; then
+    exit ${exit_code}
+  fi
+}
+
+getval() {
+  local x="${1%%=*}"
+  if [[ "$x" = "$1" ]]; then
+    echo "${2}"
+    return 2
+  else
+    echo "${1##*=}"
+    return 1
+  fi
+}
+
+usage() {
+cat <<EOF
+Usage: $0 [FLAGS] [ACTIONS]
+  FLAGS:
+    -h | --help | --usage   displays usage
+    -q | --quiet            enabled quiet mode, no output except errors
+    --debug                 enables debug mode, ignores quiet mode
+  ACTIONS:
+    create                create new Rancher & Kind cluster
+    destroy               destroy Rancher & Kind cluster created by this script
+  Examples:
+    \$ $0 create
+    \$ $0 destroy
+
+Update kind (kuberentes in docker) configuration in kind.yaml (https://kind.sigs.k8s.io/)
+EOF
+}
+
+case $(uname -s) in
+  Darwin)
+    localip="$(ipconfig getifaddr en0)"
+  ;;
+  Linux)
+    localip="$(hostname -i)"
+  ;;
+  *)
+    echo >&2 "Unsupported OS, exiting.."
+    exit 1
+  ;;
+esac
+
+## Get CLI arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -h|--help|--usage)
+      usage
+      exit 0
+    ;;
+
+    -d|--debug)
+      DEBUG=1
+      shift 1
+    ;;
+
+    -q|--quiet)
+      QUIET=1
+      shift 1
+    ;;
+
+    create|init)
+      MODE="create"
+      shift 1
+    ;;
+
+    destroy|cleanup)
+      MODE="destroy"
+      shift 1
+    ;;
+
+    *)
+      error "Unexpected option \"$1\"" -
+      usage
+      exit 1
+    ;;
+  esac
+done
+
+set -e
+
+# check docker binary availability
+if ! which docker >/dev/null; then
+  error "Docker binary cannot be found in PATH" -
+  error "Install Docker or check your PATH, exiting.."
+fi
+
+# check KIND binary availability
+if ! which kind >/dev/null; then
+  error "KIND binary is missing" -
+  error "Install it with \"go get sigs.k8s.io/kind\"" -
+  error "Or download appropriate binary from https://github.com/kubernetes-sigs/kind/releases" -
+  error "For more details see:" -
+  error " - https://kind.sigs.k8s.io" -
+  error " - https://github.com/kubernetes-sigs/kind" -
+  error "exiting.."
+fi
+
+if [[ "${MODE:-}" == "destroy" ]]; then
+  info "Destroying Rancher container.."
+  if ! docker rm -f ${RANCHER_CONTAINER_NAME}; then
+    error "failed to remove Rancher container \"${RANCHER_CONTAINER_NAME}\".." -
+  fi
+  info "Destroying Kind cluster.."
+  if ! kind delete cluster --name ${KIND_CLUSTER_NAME}; then
+    error "failed to delete Kind cluster \"${KIND_CLUSTER_NAME}\".." -
+  fi
+  exit 0
+elif [[ "${MODE:-}" != "create" ]]; then
+  usage
+  exit 0
+fi
+
+# Launch Rancher server
+if [[ $(docker ps -f name=${RANCHER_CONTAINER_NAME} -q | wc -l) -ne 0 ]]; then
+  error "Rancher container already present, delete it before trying again, exiting.."
+fi
+info "Launching Rancher container"
+if docker run -d \
+              --restart=unless-stopped \
+              --name ${RANCHER_CONTAINER_NAME}  \
+              -p ${RANCHER_HTTP_HOST_PORT}:80   \
+              -p ${RANCHER_HTTPS_HOST_PORT}:443 \
+              rancher/rancher; then
+  info "Rancher UI will be available at https://${localip}:${RANCHER_HTTPS_HOST_PORT}"
+  info "It might take few up to 60 seconds for Rancher UI to become available.."
+  info "While it's coming up, going to start KIND cluster"
+fi
+
+# Start KIND cluster
+if [[ $(kind get clusters | grep -c ${KIND_CLUSTER_NAME}) -ne 0 ]]; then
+  warn "KIND cluster is already running.."
+  echo >&2 -n "Use running KIND? [y/N] "
+  read use_running_kind
+  case $use_running_kind in
+    n|N)
+      echo >&2 "exiting.."
+      exit 1
+    ;;
+    y|Y)
+      echo >&2 "ok, continuing.."
+    ;;
+    *)
+      error "unrecognized option, exiting.."
+    ;;
+  esac
+else
+  info "Creating Kind cluster with 3 nodes (default)..."
+  kind create cluster --name ${KIND_CLUSTER_NAME} --config kind.yaml
+fi
+
+cat >&2 <<EOM
+### Next steps ###
+- Setup admin credentials in Rancher UI
+- Set "Rancher Server URL" to "https://${localip}:${RANCHER_HTTPS_HOST_PORT}" (should already be selected)
+  you may change it at any time in "Settings"
+- wait for 2 minute
+- Import KIND cluster to Rancher (via https://${localip}:${RANCHER_HTTPS_HOST_PORT}/g/clusters/add?provider=import)
+  (select "Import Existing cluster" when adding a cluster)
+  > To work around "Unable to connect to the server: x509: certificate signed by unknown authority"
+  > use "curl --insecure" which is provided by Rancher UI to get the manigest, piping it's output to, for example:
+
+    curl --insecure -sfL https://192.168.1.100:33486/v3/import/6qbm7q9lk7gmqsgt4l2hrrchlxbfh6fjskzb8tx84mjrl9jvhb8xcm.yaml | kubectl apply -f -
+
+- set context to kind cluster 
+
+kubectl cluster-info --context kind-kind-for-rancher
+
+### Destroy
+To shut everything down, use "$0 destroy", or manually with
+docker rm -f ${RANCHER_CONTAINER_NAME}; kind delete cluster ${KIND_CLUSTER_NAME}
+EOM
+
+# Open Rancher UI in browser
+open https://${localip}:${RANCHER_HTTPS_HOST_PORT}
